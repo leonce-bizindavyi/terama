0c8045dfe80fd66529f1f16d3415c3e4469fec8d
leonce-bizindavyi <leonceb900@gmail.com>
2024-03-25 13:55:23 +0200

    first commit

--- src/Config/db4.js
+++ src/Config/db4.js
@@ -1,20 +1,20 @@
 import { createPool } from 'mysql2/promise';
 
-/*const pool = createPool({
+const pool = createPool({
   host: 'www.db4free.net',
   user: 'terama_20819u',
   password: 'Promotion@11',
   database: 'terama_20819u',
   connectionLimit: 50, // Nombre maximal de connexions
  });
- */
- const pool = createPool({
+/* const pool = createPool({
   host: 'localhost',
   user: 'root',
   password: '',
   database: 'teramaflix',
   connectionLimit: 50, // Nombre maximal de connexions
  });
+ */
 // Vérifiez si la connexion est ouverte avant d'exécuter une requête
 const executeQuery = async (query, params) => {
   let connection;
